e1dd8b7aeafa89ffa4fe9795c83c4107e949cded
DarknesGaming <epicdarknesgamer@gmail.com>
2016-09-24 13:05:55 -0500

    Version 0.2.1 Upgrade
    Version 0.2.0 increases support for Spigot, and adds support for Paper
    Spigot and CraftBukkit.
    Also adds some color.
    Version 0.2.1 fixes an issue where selecting any option for Spigot and
    CraftBukkit immediately defaults to the first option.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 -----------------------------------------------------------------
 
-This is just a simple Windows batch application that will allow you to select which Minecraft server version you want to use. Only works on Windows.
+This is just a simple Windows batch application that will allow you to select which Minecraft server version you want to use. Because this is a batch application, this will NOT run on Mac OS X (as of Fall 2016, macOS), UNIX or Linux operating systems.
 
 To avoid copyright issues, I do NOT provide any jar files that allow this tool to work! You absolutely MUST supply those yourself. I prefer to maintain this project rather than get it disabled by GitHub for DMCA complaints.
 
@@ -14,8 +14,31 @@ The following format is acceptable:
 
 spigot-1.10.2.jar
 
+craftbukkit.1.10.2.jar
+
+These are just examples. However, only the latest versions of any major Minecraft release (Ex: 1.8) are currently supported.
+
 The jar files must follow this format, or the tool will not work.
 
-The current version of this program is 0.1.0.
+The current version of this program is 0.2.1.
 
-The tool only works with Spigot and Minecraft versions 1.9.4 and 1.10.2.
+The tool does not yet support Cauldron, or Plain Vanilla yet.
+
+## Usefulness
+
+-----------------------------------------------------------------
+
+This batch file is SUPER useful if you have more than one version of a jar file for a Minecraft server, and you don't want to have to change the code. The file I created eliminates that problem 99.9% of the time. You having an outdated version of a major release (1.8.7, 1.9.2, 1.10) is the .1%.
+
+## Problems
+
+-----------------------------------------------------------------
+
+If you are satisfied with my work, then I have done something right. If you have issues with this, please open an issue ticket describing what went wrong.
+
+Do NOT open any tickets for these reasons:
+* Outdated version of a major release not supported.
+* Constant crashing
+* Not working on any other OS but Windows (I made a VERY clear warning about this!)
+
+Any ticket that is opened for those reasons will be closed immediately.
--- start.bat
+++ start.bat
@@ -4,31 +4,124 @@
 
 :menu
 cls
+color f
 echo ----------------------------------------------
-echo Minecraft Server Version Selector v0.1.0
+echo Minecraft Server Version Select
+echo Version 0.2.1
 echo ----------------------------------------------
 echo.
-echo Only supports Spigot.
-echo Press One to operate on Minecraft 1.9.4
-echo Press Two to operate on Minecraft 1.10.2
-echo Press Three to exit.
+echo Press One to operate on CraftBukkit.
+echo Press Two to operate on Spigot.
+echo Press Three to operate on Paper Spigot.
+echo Press Four to operate on Cauldron (Minecraft Forge Server).
+echo Press Five to operate on plain Vanilla.
+echo Press Six to exit.
 echo.
-echo You must have the jar files ready for this to work.
 SET INPUT=
-SET /P INPUT=Select a option:
+SET /P INPUT=Select an option:
 
-IF /I '%INPUT%'=='1' GOTO option
-IF /I '%INPUT%'=='2' GOTO option2
-IF /I '%INPUT%'=='3' GOTO exit
+IF /I '%INPUT%'=='1' GOTO craftbukkit
+IF /I '%INPUT%'=='2' GOTO spigot
+IF /I '%INPUT%'=='3' GOTO paperspigot
+IF /I '%INPUT%'=='4' GOTO cauldron
+IF /I '%INPUT%'=='5' GOTO vanilla
+IF /I '%INPUT%'=='6' GOTO exit
 
-:option
-echo Now running on Minecraft 1.9.4 (Spigot)
-java -Xms512M -Xmx1536M -jar spigot-1.9.4.jar
+:craftbukkit
+cls
+color 9
+echo ----------------------------------------------
+echo CraftBukkit Version Selection
+echo ----------------------------------------------
+echo.
+echo Press One if you want to run Minecraft 1.8.8.
+echo Press Two if you want to run Minecraft 1.9.4.
+echo Press Three if you want to run Minecraft 1.10.2.
+echo Press Four to return to the menu.
+echo.
+SET INPUT=
+SET /P INPUT=Select your MC Version:
+
+IF /I '%INPUT%'=='1' GOTO craftbukkit1
+IF /I '%INPUT%'=='2' GOTO cb2
+IF /I '%INPUT%'=='3' GOTO cbthree
+IF /I '%INPUT%'=='4' GOTO menu
+
+:craftbukkit1
+echo Now running on CraftBukkit MC 1.8.8.
+echo WARNING: Make sure your jar file has this name: craftbukkit-1.8.8
+echo If the jar file does not have that name, we can't run it.
+java -Xms1024M -Xmx1024M -jar craftbukkit-1.8.8.jar
+PAUSE
 GOTO menu
 
-:option2
-echo Now running on Minecraft 1.10.2 (Spigot)
+:cb2
+echo Now running on CraftBukkit MC 1.9.4.
+echo WARNING: Make sure your jar file has this name: craftbukkit-1.9.4
+echo If the jar file does not have that name, we can't run it.
+java -Xms1024M -Xmx1024M -jar craftbukkit-1.9.4.jar
+PAUSE
+GOTO menu
+
+:cbthree
+echo Now running on CraftBukkit MC 1.10.2.
+echo WARNING: Make sure your jar file has this name: craftbukkit-1.10.2
+echo If the jar file does not have that name, we can't run it.
+java -Xms1024M -Xmx1024M -jar craftbukkit-1.10.2.jar
+PAUSE
+GOTO menu
+
+:spigot
+cls
+color 6
+echo ----------------------------------------------
+echo Spigot Version Selection
+echo ----------------------------------------------
+echo.
+echo Press A if you want to run Minecraft 1.8.8.
+echo Press B if you want to run Minecraft 1.9.4.
+echo Press C if you want to run Minecraft 1.10.2.
+echo Press D to return to the menu.
+echo.
+SET INPUT=
+SET /P INPUT=Select your MC Version:
+
+IF /I '%INPUT%'=='A' GOTO spigot1
+IF /I '%INPUT%'=='B' GOTO spigot2
+IF /I '%INPUT%'=='C' GOTO spigot3
+IF /I '%INPUT%'=='D' GOTO menu
+
+:spigot1
+echo Now running on Spigot MC 1.8.8.
+echo WARNING: Make sure your jar file has this name: spigot-1.8.8
+echo If the jar file does not have that name, we can't run it.
+java -Xms512M -Xmx1536M -jar spigot-1.8.8.jar
+PAUSE
+GOTO menu
+
+:spigot2
+echo Now running on Spigot MC 1.9.4.
+echo WARNING: Make sure your jar file has this name: spigot-1.9.4
+echo If the jar file does not have that name, we can't run it.
+java -Xms512M -Xmx1536M -jar spigot-1.9.4.jar
+PAUSE
+GOTO menu
+
+:spigot3
+echo Now running on Spigot MC 1.10.2.
+echo WARNING: Make sure your jar file has this name: spigot 1.10.2
+echo If the jar file does not have that name, we can't run it.
 java -Xms512M -Xmx1536M -jar spigot-1.10.2.jar
+PAUSE
+GOTO menu
+
+:paperspigot
+echo Now running your build of Paper Spigot.
+echo WARNING: Make sure your jar file has this name: paperclip.jar
+echo Update often from the following link:
+echo https://ci.destroystokyo.com/job/PaperSpigot/
+java -Xms512M -Xmx1536M -jar paperclip.jar
+PAUSE
 GOTO menu
 
 :exit
